84c48a495c0c6c782d3ef81f5df94aa7eab2e55c
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2022-04-13 06:37:35 -0300

    Projeto criado

---
+++ Challenge_Back-End_3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Challenge_Back_End_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
