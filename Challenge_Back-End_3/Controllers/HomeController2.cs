615eda79080cf345ccf96c8c856737b8cd3504be
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2022-04-18 23:51:01 -0300

    Salva arquivo e imprime nome/tamanho

---
+++ Challenge_Back-End_3/Controllers/HomeController2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_Back_End_3.Controllers
+{
+    public class HomeController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Challenge_Back-End_3/Controllers/UploadController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenge_Back_End_3.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly string wwwrootDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+
+        public IActionResult Index()
+        {
+            List<string> arquivo = Directory.GetFiles(wwwrootDirectory, "*.txt").Select(Path.GetFileName).ToList();
+            return View(arquivo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(IFormFile myFile)
+        {
+            if (myFile != null)
+            {
+                var path = Path.Combine(wwwrootDirectory, myFile.FileName
+                    + Path.GetExtension(myFile.FileName));
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await myFile.CopyToAsync(stream);
+                }
+                Console.WriteLine("Nome do arquivo: " + myFile.FileName + " Tamanho do arquivo: " + myFile.Length/1024/1024 + "MB");              
+            }
+
+            return View();
+        }
+    }
+}
